64ce3651439f3481e5a58cab90db569c64155138
Tadeusz K. <tadeusz.kondracki@gmail.com>
2026-06-10 21:45:53 +0200

    map display

---
+++ src/lib/components/TenkiMap.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import type { LngLatBoundsLike } from 'svelte-maplibre';
+  import { MapLibre } from 'svelte-maplibre';
+
+  const maxbounds: LngLatBoundsLike = [5.02, 45.5, 11.5, 48];
+</script>
+
+<MapLibre 
+  center={[8.2,46.8]}
+  zoom={8}
+  class="map"
+  standardControls
+  maxBounds={maxbounds}
+  style="https://vectortiles.geo.admin.ch/styles/ch.swisstopo.lightbasemap.vt/style.json" />
+
+<style>
+  :global(.map) {
+    height: 100vh;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to Tenki Travel</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import TenkiMap from '$lib/components/TenkiMap.svelte';
+</script>
+
+<TenkiMap/>
